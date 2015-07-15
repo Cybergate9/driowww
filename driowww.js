765aa21fb2423c0363036874e86645b659fec1d6
Shaun Osborne <shaun@cybergate9.net>
2015-07-15 07:09:59 +0100

    styles tidy up'

--- driowww.js
+++ driowww.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var logger = require('morgan');
+var md = require('node-markdown').Markdown;
 var fs = require('fs');
-//var jade = require('jade');
+
 
 var app=express();
 
-//
-app.locals.pretty = true;
+//app.locals.pretty = true;
 
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/files'));
@@ -14,15 +14,26 @@ app.use(express.static(__dirname + '/files'));
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 app.use(logger('combined', {stream: accessLogStream}));
 
-app.get(['/','/blog','/home','/contact'], function(req, res){
+app.get(['/','/contact'], function(req, res){
 	res.render('index', { title: 'datarefinery.io', message: 'datarefinery.io coming soon!'});
 });
-
- // Handle 404
-  app.use(function(req, res) {
-      res.status(400);
-      res.render('404.jade', {title: '404: File Not Found'});
+app.get('/blog', function(req, res) {
+	    var files = fs.readdirSync('blog');
+			var mddata = [];
+			for(idx in files){
+			if(files[idx][0] !== '.'){
+      mddata.push(fs.readFileSync('blog/'+files[idx], {encoding: 'utf-8'}));
+		 }
+		}
+		res.render('blog', {title: 'Welcome to the Bloggery', message: mddata, md: md});
+});
+// Handle 404
+app.use(function(req, res) {
+  res.status(400);
+  res.render('404.jade', {
+    title: '404: File Not Found'
   });
+});
 
 
 var server = app.listen(80, function(){
